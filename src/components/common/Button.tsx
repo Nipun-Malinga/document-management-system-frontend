4497f68cccc2a85a7b64b4d49d3b1dcf832eda65
Nipun Malinga <nipunmalinga11@gmail.com>
2025-10-16 20:10:12 +0530

    feature: implement document card component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,7 +1,7 @@
 import type { IconType } from 'react-icons';
 
 interface props {
-  title: string;
+  title?: string;
   type: 'button' | 'reset' | 'submit';
   icon?: IconType;
   onClick: () => void;
@@ -12,10 +12,10 @@ const Button = ({ title, type, icon: Icon, onClick }: props) => {
     <button
       type={type}
       onClick={() => onClick()}
-      className='bg-gray-800 text-white sm:text-xs md:text-lg px-3 py-1 flex items-center justify-center gap-x-2 rounded-sm cursor-pointer transition duration-100 ease-in-out hover:bg-gray-600 active:bg-gray-800'
+      className='bg-gray-800 text-white text-xs md:text-lg px-2 py-1 flex items-center justify-center gap-x-2 rounded-sm cursor-pointer transition duration-100 ease-in-out hover:bg-gray-600 active:bg-gray-800'
     >
       {Icon && <Icon />}
-      {title}
+      {title && title}
     </button>
   );
 };
---
+++ src/components/common/DocumentCard.tsx
@@ -0,0 +1,31 @@
+import { LuFileText } from 'react-icons/lu';
+import { HiDotsVertical } from 'react-icons/hi';
+import Button from './Button';
+
+interface Props {
+  title: string;
+  createdDate: string;
+  updatedDate: string;
+}
+
+const DocumentCard = ({ title, createdDate, updatedDate }: Props) => {
+  return (
+    <div className='group w-full min-h-[16rem] p-2.5 border border-gray-200 rounded-lg flex flex-col justify-around transition-all duration-200 ease-in-out hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+      <div className='relative bg-blue-50 w-full min-h-[10rem] rounded-md flex justify-center items-center'>
+        <div className='absolute top-2 right-2 opacity-0 transition group-hover:opacity-100'>
+          <Button icon={HiDotsVertical} type='button' onClick={() => {}} />
+        </div>
+        <LuFileText className='text-8xl text-blue-600' />
+      </div>
+      <div className='flex flex-col justify-center'>
+        <p className='text-xl capitalize font-medium'>{title}</p>
+        <div className='flex justify-between'>
+          <p className='text-sm text-gray-500'>Created: {createdDate}</p>
+          <p className='text-sm text-gray-500'>Updated: {updatedDate}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentCard;
