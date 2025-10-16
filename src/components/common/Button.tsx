951be709c06df3e922075b0740dbc9ce92d3e014
Nipun Malinga <nipunmalinga11@gmail.com>
2025-10-16 10:44:09 +0530

    feature: implement button component. - add react icons dependency.

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,23 @@
+import type { IconType } from 'react-icons';
+
+interface props {
+  title: string;
+  type: 'button' | 'reset' | 'submit';
+  icon?: IconType;
+  onClick: () => void;
+}
+
+const Button = ({ title, type, icon: Icon, onClick }: props) => {
+  return (
+    <button
+      type={type}
+      onClick={() => onClick()}
+      className='bg-gray-800 text-white sm:text-xs md:text-lg px-3 py-1 flex items-center justify-center gap-x-2 rounded-sm cursor-pointer transition duration-100 ease-in-out hover:bg-gray-600 active:bg-gray-800'
+    >
+      {Icon && <Icon />}
+      {title}
+    </button>
+  );
+};
+
+export default Button;
