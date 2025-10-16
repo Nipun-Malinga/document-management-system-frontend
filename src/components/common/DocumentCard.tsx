438a1ca9f5142f3ba5c8fcbc4234e9da84b875db
Nipun Malinga <nipunmalinga11@gmail.com>
2025-10-17 02:45:51 +0530

    feature: add badge component to the DocumentCard

--- src/components/common/DocumentCard.tsx
+++ src/components/common/DocumentCard.tsx
@@ -1,14 +1,16 @@
 import { LuFileText } from 'react-icons/lu';
 import { HiDotsVertical } from 'react-icons/hi';
 import Button from './Button';
+import Badge from './Badge';
 
 interface Props {
   title: string;
+  state: string;
   createdDate: string;
   updatedDate: string;
 }
 
-const DocumentCard = ({ title, createdDate, updatedDate }: Props) => {
+const DocumentCard = ({ title, state, createdDate, updatedDate }: Props) => {
   return (
     <div className='group w-full min-h-[16rem] p-2.5 border border-gray-200 rounded-lg flex flex-col justify-around transition-all duration-200 ease-in-out hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
       <div className='relative bg-blue-50 w-full min-h-[10rem] rounded-md flex justify-center items-center'>
@@ -16,6 +18,12 @@ const DocumentCard = ({ title, createdDate, updatedDate }: Props) => {
           <Button icon={HiDotsVertical} type='button' onClick={() => {}} />
         </div>
         <LuFileText className='text-8xl text-blue-600' />
+        <div className='absolute bottom-2 right-2'>
+          <Badge
+            text={state}
+            theme={state.toLowerCase() == 'public' ? 'primary' : 'secondary'}
+          />
+        </div>
       </div>
       <div className='flex flex-col justify-center'>
         <p className='text-xl capitalize font-medium'>{title}</p>
