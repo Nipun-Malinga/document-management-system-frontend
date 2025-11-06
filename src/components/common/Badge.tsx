524af5dd863ca83d839fd84741d0a78eaf1870fa
Nipun Malinga <nipunmalinga11@gmail.com>
2025-11-06 09:04:43 +0530

    refactor: modify the badge component and the document grid layout

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,21 +1,19 @@
 import type { ThemeVariant } from '../../themes/themes';
 import { themeColors } from '../../themes/themes';
+import { Badge as Bdg } from '../ui/badge';
 
 interface Props {
   text: String;
   theme: ThemeVariant;
-  onClick?: () => void;
 }
 
-const Badge = ({ text, theme, onClick }: Props) => {
+const Badge = ({ text, theme }: Props) => {
   return (
-    <div
-      className={`${themeColors[theme].background} ${themeColors[theme].fontColor}  h-full py-1 px-2 rounded-sm flex justify-center items-center shadow-[0_3px_10px_rgb(0,0,0,0.3)]`}
+    <Bdg
+      className={`${themeColors[theme].background} ${themeColors[theme].fontColor} font-bold w-15 rounded-sm`}
     >
-      <p className='text-center text-xs font-bold' onClick={onClick && onClick}>
-        {text}
-      </p>
-    </div>
+      {text}
+    </Bdg>
   );
 };
 
